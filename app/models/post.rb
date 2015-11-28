5596f31d7f6eb084910c4ff21738a188bd1b8ed2
TAmyG <tamy.vivas@gmail.com>
2015-11-28 15:48:37 -0600

    mejoras en las vistas de los post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ActiveRecord::Base
-  belongs_to :usuario
+  #dependent destroy elimina todas las referencias en todos los
+  #modelos relacionado
+  belongs_to :usuario, dependent: :destroy
   has_many :attachments
   validates :titulo, presence: true, uniqueness: true
+  before_save :valores_por_default
   include Picturable
   include PublicActivity::Model
   	tracked owner: Proc.new{ |controller, model| controller.current_usuario }
+
+#el || sirve para que rails asigne el valor si se crea el modelo sin dicho campo
+  	def valores_por_default
+  		self.costo ||= 0
+  	end
 end
