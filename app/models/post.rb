cf3cb7c8a4487d3b1146abb994bfb6987765b61d
TAmyG <tamy.vivas@gmail.com>
2015-11-29 19:03:30 -0600

    configuracion y publish en redis de cada post antes de crearlo

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ActiveRecord::Base
   validates :titulo, presence: true, uniqueness: true  
 
   before_save :valores_por_default
+  after_create {|post| post.message 'create'}
+
   include Picturable
   include PublicActivity::Model
   	tracked owner: Proc.new{ |controller, model| controller.current_usuario }
@@ -20,4 +22,15 @@ class Post < ActiveRecord::Base
       self.payments.where(estado: 2).size > 0
     end
 
+    def message action
+      msg = {
+        resource: 'posts',
+        action: action,
+        id: self.id,
+        obj: self,
+        username: self.usuario.username.capitalize,
+        user_id: self.usuario.id
+      }
+      $redis.publish 'rt-change', msg.to_json
+    end
 end
