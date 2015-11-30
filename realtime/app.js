73e46f7bf24e195367053a1ce9b40953c72b0908
TAmyG <tamy.vivas@gmail.com>
2015-11-29 18:49:54 -0600

    creacion de dependencias en package.json para redis y socket.io y configuracion de los mismos en archivo app.js

---
+++ realtime/app.js
@@ -0,0 +1,17 @@
+var io = require('socket.io').listen(5001);
+
+var redis = require('redis');
+
+var redis_client = redis.createClient();
+
+redis_client.subscribe('rt-change');
+io.configure(function(){
+
+
+});
+
+io.on('connection', function(socket){
+	redis_client.on('message', function(channel, message){
+		socket.emit('rt_change', JSON.parse(message));
+	});
+});
